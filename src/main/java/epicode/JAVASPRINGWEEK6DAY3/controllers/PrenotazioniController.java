15923df860da65fbd15479384778e1e2323f7481
VittoriaBasile <vichy.basile@gmail.com>
2023-06-07 19:46:19 +0200

    commit

--- src/main/java/epicode/JAVASPRINGWEEK6DAY3/controllers/PrenotazioniController.java
+++ src/main/java/epicode/JAVASPRINGWEEK6DAY3/controllers/PrenotazioniController.java
@@ -32,6 +32,22 @@ public class PrenotazioniController {
 		return prenotazioniService.find(page, size, sortBy);
 	}
 
+	@GetMapping("/info")
+	public String info() {
+		return "Inserisci una lingua scegliendo tra italiano e inglese";
+	}
+
+	@GetMapping("/info/{lingua}")
+	public String infoIta(@PathVariable String lingua) {
+		if (lingua.equals("ita")) {
+			return "Benvenut nel sistema di prenotazioni delle postazioni di lavoro; puoi scegliere in base alla città , all'edificio e al tipo di postazione richiesta.Ricorda che ogni prenotazione effettuata è valida per 24 ore.";
+		} else if (lingua.equals("eng")) {
+			return "Welcome to the workstation reservation system; you can choose according to the city, the building and the type of seat requested. Remember that every reservation made is valid for 24 hours.";
+		} else {
+			return "Inserisci una lingua scegliendo tra italiano e inglese";
+		}
+	}
+
 	@GetMapping("/{prenotazioneId}")
 	public Prenotazione getPrenotazione(@PathVariable UUID prenotazioneId) throws Exception {
 		return prenotazioniService.findById(prenotazioneId);
