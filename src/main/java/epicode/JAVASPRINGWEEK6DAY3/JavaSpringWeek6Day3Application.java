5f514b4c003b1bad37b8503d2b4169bfb19d6e12
VittoriaBasile <vichy.basile@gmail.com>
2023-06-07 13:38:22 +0200

    first commit

---
+++ src/main/java/epicode/JAVASPRINGWEEK6DAY3/JavaSpringWeek6Day3Application.java
@@ -0,0 +1,13 @@
+package epicode.JAVASPRINGWEEK6DAY3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWeek6Day3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWeek6Day3Application.class, args);
+	}
+
+}
---
+++ src/test/java/epicode/JAVASPRINGWEEK6DAY3/JavaSpringWeek6Day3ApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.JAVASPRINGWEEK6DAY3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringWeek6Day3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
